9911c3e4ac0c60e08f35d026504386d6bcdce2b2
Yousef Elsayed <yousef4511@outlook.com>
2024-03-17 14:32:27 +0200

    Finish todo app form

---
+++ lab-2/todo-app/src/Form.jsx
@@ -0,0 +1,35 @@
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import { useRef } from "react";
+
+export default function ToDoForm() {
+  let todo = useRef("");
+
+  function handleInput() {
+    console.log(todo.current.value);
+  }
+  return (
+    <>
+      <InputGroup className="mb-3">
+        <Button
+          variant="outline-secondary"
+          id="button-addon1"
+          onClick={handleInput}
+        >
+          ToDo
+        </Button>
+        <Form.Control
+          aria-label="Example text with button addon"
+          aria-describedby="basic-addon1"
+          ref={todo}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              handleInput();
+            }
+          }}
+        />
+      </InputGroup>
+    </>
+  );
+}
